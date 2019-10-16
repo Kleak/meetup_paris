e79b93ea9a087231adb98a7118a41dbec97c8a98
kevin <segaud.kevin@gmail.com>
2019-10-16 15:09:59 +0200

    fix default target platform
    Signed-off-by: kevin <segaud.kevin@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:meetup_paris/src/app/app.dart';
 
 void main() {
+  debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
+
   runApp(App());
 }
